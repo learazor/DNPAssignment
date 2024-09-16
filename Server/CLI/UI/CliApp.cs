38d9ddbd681267b0d3110cc747522d981afe80c0
razor <razor.srt@gmail.com>
2024-09-16 21:16:19 +0200

    assignment 2

---
+++ Server/CLI/UI/CliApp.cs
@@ -0,0 +1,132 @@
+using Entities;
+using RepositoryContracts;
+
+namespace CLI.UI;
+
+public class CliApp
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IPostRepository _postRepository;
+    private readonly ICommentRepository _commentRepository;
+
+    public CliApp(IUserRepository userRepository, ICommentRepository commentRepository, IPostRepository postRepository)
+    {
+        _userRepository = userRepository;
+        _postRepository = postRepository;
+        _commentRepository = commentRepository;
+    }
+    
+    public async Task StartAsync()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Create User");
+            Console.WriteLine("2. Create Post");
+            Console.WriteLine("3. Add Comment");
+            Console.WriteLine("4. View Posts Overview");
+            Console.WriteLine("5. View Specific Post");
+            Console.WriteLine("6. Exit");
+
+            var input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    await CreateUserAsync();
+                    break;
+                case "2":
+                    await CreatePostAsync();
+                    break;
+                case "3":
+                    await AddCommentAsync();
+                    break;
+                case "4":
+                    ViewPostsOverview();
+                    break;
+                case "5":
+                    await ViewSpecificPostAsync();
+                    break;
+                case "6":
+                    return;
+                default:
+                    Console.WriteLine("You can choose options from 1 to 6.");
+                    break;
+            }
+        }
+    }
+    
+        private async Task CreateUserAsync()
+    {
+        Console.Write("Enter username: ");
+        var username = Console.ReadLine();
+        Console.Write("Enter password: ");
+        var password = Console.ReadLine();
+
+        var user = new User(username, password);
+        await _userRepository.AddAsync(user);
+        Console.WriteLine($"User created with ID: {user.Id}");
+    }
+
+    private async Task CreatePostAsync()
+    {
+        Console.Write("Enter title: ");
+        var title = Console.ReadLine();
+        Console.Write("Enter body: ");
+        var body = Console.ReadLine();
+        Console.Write("Enter user ID: ");
+        var userId = int.Parse(Console.ReadLine());
+
+        var post = new Post (title, body, userId );
+        await _postRepository.AddAsync(post);
+        Console.WriteLine($"Post created with ID: {post.Id}");
+    }
+
+    private async Task AddCommentAsync()
+    {
+        Console.Write("Enter comment: ");
+        var body = Console.ReadLine();
+        Console.Write("Enter user ID: ");
+        var userId = int.Parse(Console.ReadLine());
+        Console.Write("Enter post ID: ");
+        var postId = int.Parse(Console.ReadLine());
+
+        var comment = new Comment(body, userId, postId);
+        await _commentRepository.AddAsync(comment);
+        Console.WriteLine($"Comment added to Post ID: {postId}");
+    }
+
+    private void ViewPostsOverview()
+    {
+        var posts = _postRepository.GetMany();
+        foreach (var post in posts)
+        {
+            Console.WriteLine($"ID: {post.Id}, Title: {post.Title}");
+        }
+    }
+
+    private async Task ViewSpecificPostAsync()
+    {
+        Console.Write("Enter post ID: ");
+        var postId = int.Parse(Console.ReadLine());
+
+        var post = await _postRepository.GetSingleAsync(postId);
+        if (post == null)
+        {
+            Console.WriteLine("Post not found");
+            return;
+        }
+
+        Console.WriteLine($"Title: {post.Title}");
+        Console.WriteLine($"Body: {post.Body}");
+
+        var comments = _commentRepository.GetMany();
+        foreach (var comment in comments)
+        {
+            Console.WriteLine($"Comment by User {comment.UserId}: {comment.Body}");
+        }
+    }
+    
+}
+
+
+    
---
+++ Server/CLI/UI/ManageUsers/CreateUserView.cs
@@ -0,0 +1,13 @@
+using RepositoryContracts;
+
+namespace CLI.UI.ManageUsers;
+
+public class CreateUserView
+{
+    private readonly IUserRepository userRepository;
+
+    public CreateUserView(IUserRepository userRepository)
+    {
+        this.userRepository = userRepository;
+    }
+}
